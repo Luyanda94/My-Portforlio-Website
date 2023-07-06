6f224a743601de19220e9c131caaa7c1b0ede1dc
Luyanda <125657799+Luyanda94@users.noreply.github.com>
2023-07-06 08:03:03 -0800

    Add files via upload

---
+++ My Website Portfolio/The Creative Dev/js.js
@@ -0,0 +1,17 @@
+//[08:27] Stephan Williams
+/*Interview Question
+
+Problem Statement : We receive a sentence as input parameter and 
+need you write a method that returns the every alternating word in that sentence in the reversed order they came in
+Example Input( I love coding very much)
+Example Response ( much coding I )*/
+
+function reverse_order(string sentence) {
+    a = [1,2,3,4,5]
+    for (int b=0, )
+
+
+}
+
+
+
